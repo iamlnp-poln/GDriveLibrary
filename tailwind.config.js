8183a971853586f29c82a98611332bc48fb82d56
Lai Nhat Phong <waytothefuture07@gmail.com>
2026-02-23 03:49:27 +0700

    Ghi đè code server bằng bản local mới nhất

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Plus Jakarta Sans', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+};
